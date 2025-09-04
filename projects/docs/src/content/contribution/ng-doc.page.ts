a0b17946a2adbe0d1b07da2d30bafb0f7a11f68c
Piotr Liszka <piotr.liszka@hotmail.com>
2025-09-04 22:42:23 +0200

    feat: crafting documentation part 2

---
+++ projects/docs/src/content/contribution/ng-doc.page.ts
@@ -0,0 +1,9 @@
+import {NgDocPage} from '@ng-doc/core';
+
+const ContributionPage: NgDocPage = {
+	title: `Contribution`,
+	mdFile: './index.md',
+  order: 90
+};
+
+export default ContributionPage;
---
+++ projects/docs/src/content/demo/ng-doc.category.ts
@@ -0,0 +1,8 @@
+import {NgDocCategory} from '@ng-doc/core';
+
+const DemoCategory: NgDocCategory = {
+	title: 'Live Demo',
+  order: 5
+};
+
+export default DemoCategory;
